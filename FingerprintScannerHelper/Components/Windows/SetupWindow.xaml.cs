ee71deae9012b60a428a250c267a7db85f97ca92
MADEY-OS <madej256@gmail.com>
2023-01-25 19:46:23 +0100

    Init

---
+++ FingerprintScannerHelper/Components/Windows/SetupWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows;
+
+namespace FingerprintScannerHelper.Components.Windows
+{
+    /// <summary>
+    /// Interaction logic for SetupWindow.xaml
+    /// </summary>
+    public partial class SetupWindow : Window
+    {
+        public SetupWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSearchFolder_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                Uri fileUri = new Uri(openFileDialog.FileName);
+            }
+        }
+    }
+}
---
+++ FingerprintScannerHelper/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using FingerprintScannerHelper.Components.Windows;
+using System.Windows;
+
+namespace FingerprintScannerHelper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Arduino_MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            ArduinoSetupWindow arduinoSetupWindow = new ArduinoSetupWindow();
+            arduinoSetupWindow.Show();
+        }
+        private void Help_MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            HelpWindow helpWindow = new HelpWindow();
+            helpWindow.Show();
+        }
+    }
+}
